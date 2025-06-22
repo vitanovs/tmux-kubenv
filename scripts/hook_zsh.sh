f60e79e54df183027bfa9d75778a6e052a8ea190
Stoyan Vitanov <stoyan.a.vitanov@gmail.com>
2025-06-22 10:52:27 +0300

    feat(scripts/hook/zsh): Add `enable/disable` script for `precmd_functions` hook
    This commit adds a script that features `enable/disable` utilities
    of `ZSH` hook.
    
    The featured `hook` is executed `pre`-command and updates a `tmux`
    option that tracks the currently exported `KUBECONFIG`.

---
+++ scripts/hook_zsh.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+OPTION_KUBECONFIG="@tmux_kubenv_kubeconfig"
+
+tmux_kubenv_precmd_hook() {
+	# Update option with the current KUBECONFIG value
+	tmux set-option -gp "$OPTION_KUBECONFIG" "$KUBECONFIG"
+}
+
+tmux_kubenv_precmd_hook_enable() {
+	if [[ "${precmd_functions[@]}" =~ "tmux_kubenv_precmd_hook" ]]; then
+		echo "Hook already enabled"
+		return 0
+	fi
+	precmd_functions+=("tmux_kubenv_precmd_hook")
+	echo "Hook enabled"
+}
+
+tmux_kubenv_precmd_hook_disable() {
+	if [[ ! "${precmd_functions[@]}" =~ "tmux_kubenv_precmd_hook" ]]; then
+		echo "Hook already disabled"
+		return 0
+	fi
+	precmd_functions=("${precmd_functions[@]/tmux_kubenv_precmd_hook}")
+	echo "Hook disabled"
+}
